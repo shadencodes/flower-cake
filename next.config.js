f926e548473006ba5d0afe38a5feebcfbdd13354
ShadenCodes <shadencodes@gmail.com>
2020-09-21 17:57:05 +0300

    Create next.config.js to make env. variables avaialbe at build time

---
+++ next.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    env: {
+        REACT_APP_SPACE_ID: process.env.REACT_APP_SPACE_ID,
+        REACT_APP_ACCESS_TOKEN: process.env.REACT_APP_ACCESS_TOKEN,
+        REACT_APP_STRIPE_KEY: process.env.REACT_APP_STRIPE_KEY
+    },
+  }
