279bd6d89ed78944b424c5913eb324492d4eebc8
ShadenCodes <shadencodes@gmail.com>
2020-09-21 17:30:43 +0300

    Fix bug: move contentful client inside componentDidMount function

--- src/pages/Store/Store.jsx
+++ src/pages/Store/Store.jsx
@@ -10,10 +10,10 @@ import {
  } from './Store.style';
 
 
-const client = (process.env.REACT_APP_SPACE_ID) ? contentful.createClient({
+/*const client = (process.env.REACT_APP_SPACE_ID) ? contentful.createClient({
     space: process.env.REACT_APP_SPACE_ID,
     accessToken: process.env.REACT_APP_ACCESS_TOKEN
-}) : null;
+}) : null;*/
 
 class Store extends React.Component{
     state = {
@@ -21,6 +21,10 @@ class Store extends React.Component{
     }
 
     componentDidMount(){
+        const client = contentful.createClient({
+            space: process.env.REACT_APP_SPACE_ID,
+            accessToken: process.env.REACT_APP_ACCESS_TOKEN
+        }) 
         client.getEntries().then( response => {
             this.setState({
                 products: response.items.map(item => item.fields)
